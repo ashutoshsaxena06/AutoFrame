0e57717f30f2777b5ae207ba4db014ceddbe70e0
Ashu-Desk <Ashu-Desk@192.168.0.102>
2018-05-31 11:57:58 +0530

    'commit'

---
+++ src/test/java/com/test/TestCheney.java
@@ -0,0 +1,51 @@
+package com.test;
+
+import com.util.framework.CommonCheney;
+
+
+public class TestCheney extends CommonCheney {
+	public static String path = System.getProperty("user.home")+"\\Downloads\\chromedriver_win32\\chromedriver.exe";
+/*
+	@BeforeClass
+	public void setup() {
+		System.out.println("*************Cheney************");
+	}
+
+	@AfterClass
+	public void AfterClass() {
+		System.out.println("************End***********");
+	}
+
+	@BeforeMethod
+	public void beforeTest() {
+		System.out.println("***********StartTest*********");
+//		RandomAction.deleteFiles(System.getProperty("user.home")+"\\Downloads");
+		driver = RandomAction.openBrowser("Chrome", path);
+		System.out.println("Invoked browser .. ");
+
+	}
+
+	@AfterMethod
+	public void pretearDown() {
+		if (driver != null) {
+			driver.quit();
+			System.out.println("*******EndTest*********");
+		}
+	}
+	
+	@Test
+	public void Agliolio_Restaurant_Cheney() throws InterruptedException {
+		System.out.println("1, Boynton_Cheney");
+		// check if login is success
+		LoginCheney(driver, "011171-01CBI", "Pasta4930");
+		// check the flow to exports
+		// StepsToExportCheney(driver);
+		// rename downloadeds
+		// String CurrentPath = RandomAction.setdownloadDir();
+		// File GFS_OG = RandomAction.getLatestFilefromDir(CurrentPath);
+	//	SendMailSSL.sendMailAction("Cheney - Offline GP", "Agliolio Boynton Italian Bistro & Bar");
+//		SendMailSSL.sendMailActionXlsx("Cheney - Offline GP", "Agliolio Restaurant");
+		
+	}
+*/
+}
