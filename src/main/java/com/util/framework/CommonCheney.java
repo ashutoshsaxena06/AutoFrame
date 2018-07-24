1e8f116e86d57efd5b1b355a4a0f2ce21a5cdf91
ashutoshsaxena06 <ashutoshsaxena06@gmail.com>
2018-07-24 10:50:11 +0530

    updated cheney

--- src/main/java/com/util/framework/CommonCheney.java
+++ src/main/java/com/util/framework/CommonCheney.java
@@ -19,6 +19,8 @@ public class CommonCheney {
 	public static WebDriver driver;
 	public static WebDriverWait wait;
 	private final static Logger logger = Logger.getLogger(CommonCheney.class);
+	public static int retry = 0;
+	public static int maxretry = 3;
 
 	public boolean LoginCheney(WebDriver driver, String usernameCBI, String passwordCBI) throws InterruptedException {
 
@@ -47,12 +49,12 @@ public class CommonCheney {
 				.elementToBeClickable(driver.findElement(By.xpath("//input[contains(@value,'Login')]"))));
 		btn_Login.click();
 
-		logger.info("Login Successful");
+		System.out.println("Login Successful");
 
 		Thread.sleep(2000);
 		// ordering
-		WebElement lnk_Ordering = wait
-				.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//a[contains(.,'Ordering')]"))));
+		WebElement lnk_Ordering = wait.until(
+				ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//a[contains(.,'Ordering')]"))));
 		lnk_Ordering.click();
 
 		Thread.sleep(2000);
@@ -61,22 +63,22 @@ public class CommonCheney {
 		try {
 			List<WebElement> allElements = driver
 					.findElements(By.xpath("//a[contains(.,'Ordering')]/following-sibling::div/ul/li/*/*/div/a"));
-			logger.info(allElements.size());
+			System.out.println(allElements.size());
 
 			for (WebElement element : allElements) {
-				logger.info(element.getText());
+				System.out.println(element.getText());
 				if (element.getText().equalsIgnoreCase("Order Guide / Entire Order Guide")
 						|| element.getText().equalsIgnoreCase("Order Guides")) {
 					String OG_text = element.getText();
 					element.click();
-					logger.info("Clicked on link - " + OG_text);
+					System.out.println("Clicked on link - " + OG_text);
 					break;
 				}
 
 			}
 		} catch (Exception e2) {
 			e2.printStackTrace();
-			logger.info("using URL for Order Guide");
+			System.out.println("using URL for Order Guide");
 			driver.get("http://www.procurement.itradenetwork.com/Platform/Products/BrowseProducts/Browse");
 		}
 		// Export grid button to show list
@@ -84,12 +86,12 @@ public class CommonCheney {
 			Thread.sleep(3000);
 			wait.until(ExpectedConditions
 					.elementToBeClickable(driver.findElement(By.xpath("//a[contains(@id,'ExportGridButton')]/span/*"))))
-			.click();
-			logger.info("Clicked - Export Grid");
+					.click();
+			System.out.println("Clicked - Export Grid");
 		} catch (NoSuchElementException e1) {
 			Thread.sleep(3000);
 			driver.findElement(By.xpath("//a[contains(@id,'ExportGridButton')]/span/*")).click();
-			logger.info("Clicked - Export Grid");
+			System.out.println("Clicked - Export Grid");
 			e1.printStackTrace();
 		}
 
@@ -103,67 +105,18 @@ public class CommonCheney {
 		WebElement ddl_Excel = wait
 				.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[contains(.,'Excel')]"))));
 		ddl_Excel.click();
-		logger.info("format choosen as Excel");
+		System.out.println("format choosen as Excel");
 		// div[@id='ExportTypeContainer']/div/ul/li[2]/a/span
 
 		// Thread.sleep(2000);
-
-		// Select columns for OG - Item no., Pack, Brand, Description, Price &
-		// caseUom
-
-		try {
-			Thread.sleep(2000);
-			ArrayList<String> removeColumns = new ArrayList<String>();
-			List<WebElement> OG_Col = driver.findElements(By.xpath("//ul[@id='Sortable']/*"));
-			logger.info(OG_Col.size());
-			String Col_id;
-			Iterator<WebElement> iterator = OG_Col.iterator();
-			while (iterator.hasNext()) {
-				WebElement element = (WebElement) iterator.next();
-				Thread.sleep(1000);
-
-				Col_id = element.getAttribute("id");
-				if (Col_id.equalsIgnoreCase("Sku") || Col_id.equalsIgnoreCase("DistributorNumber")
-						|| Col_id.equalsIgnoreCase("CaseUom") || Col_id.equalsIgnoreCase("UnitOfMeasure")
-						|| Col_id.equalsIgnoreCase("CasePack") || Col_id.equalsIgnoreCase("PackDescription")
-						|| Col_id.equalsIgnoreCase("Description")|| Col_id.equalsIgnoreCase("ProductDescription")
-						|| Col_id.equalsIgnoreCase("BrandName") || Col_id.equalsIgnoreCase("Brand")
-						|| Col_id.equals("CatalogProductStatus") || Col_id.equalsIgnoreCase("ProductStatus")
-						|| Col_id.equalsIgnoreCase("CasePrice")) {
-					logger.info("selected column :- " + Col_id);
-				}
-
-				else {
-					iterator.remove();
-					removeColumns.add(Col_id);
-				}
+		while (retry < maxretry) {
+			if (removeColumns() == true) {
+				break;
 			}
-
-			logger.info(removeColumns.size() + " and " + OG_Col.size());
-			Assert.assertEquals(OG_Col.size(), 7);
-
-			for (String column : removeColumns) {
-				wait.until(ExpectedConditions
-						.elementToBeClickable(driver.findElement(By.xpath("//li[contains(@id,'" + column + "')]"))));// remove
-				// column
-				driver.findElement(By.xpath("//li[contains(@id,'" + column + "')]/table/tbody/tr/td[2]/img")).click();
-				Thread.sleep(3000);
-				logger.info("removed column :- " + column);
-			} /*
-			 * for (int i = 0; i < removeColumns.size(); i++) {
-			 * wait.until(ExpectedConditions.elementToBeClickable(
-			 * driver.findElement(By.xpath("//li[contains(@id,'" + removeColumns.get(i) +
-			 * "')]"))));// remove // column driver.findElement(
-			 * By.xpath("//li[contains(@id,'" + removeColumns.get(i) +
-			 * "')]/table/tbody/tr/td[2]/img")) .click(); Thread.sleep(3000);
-			 * logger.info("removed column :- " + removeColumns.get(i)); }
-			 */
-		} catch (StaleElementReferenceException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			logger.info(e.getMessage());
-			e.printStackTrace();
 		}
+		List<WebElement> OG_Col = driver.findElements(By.xpath("//ul[@id='Sortable']/*"));
+		System.out.println(OG_Col.size());
+		Assert.assertEquals(OG_Col.size(), 7);
 
 		// Click Download Button
 		WebElement lnk_Download = wait.until(ExpectedConditions.elementToBeClickable(
@@ -171,9 +124,13 @@ public class CommonCheney {
 		lnk_Download.click();
 
 		// Choose Logout option
-		WebElement lnk_Logout = wait.until(
-				ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//a[contains(.,'Logout')]"))));
-		lnk_Logout.click();
+		try {
+			WebElement lnk_Logout = wait.until(
+					ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//a[contains(.,'Logout')]"))));
+			lnk_Logout.click();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 		Thread.sleep(3000);
 		return true;
@@ -207,7 +164,7 @@ public class CommonCheney {
 				.elementToBeClickable(driver.findElement(By.xpath("//input[contains(@value,'Login')]"))));
 		btn_Login.click();
 
-		logger.info("Login Successful");
+		System.out.println("Login Successful");
 
 		Thread.sleep(2000);
 		// ordering
@@ -220,7 +177,7 @@ public class CommonCheney {
 		// **** Custom Order Guide Selection ***
 		List<WebElement> allElements = driver
 				.findElements(By.xpath("//a[contains(.,'Ordering')]/following-sibling::div/ul/li/*/*/div/a"));
-		logger.info(allElements.size());
+		System.out.println(allElements.size());
 		Thread.sleep(1000);
 
 		for (WebElement element : allElements) {
@@ -228,7 +185,7 @@ public class CommonCheney {
 			if (element.getText().equalsIgnoreCase("Custom Order Guides")) {
 				String OG_text = element.getText();
 				element.click();
-				logger.info("Clicked on link - " + OG_text);
+				System.out.println("Clicked on link - " + OG_text);
 				break;
 			}
 
@@ -241,17 +198,17 @@ public class CommonCheney {
 		// Export grid button to show list
 		try {
 			driver.findElement(By.xpath("//a[contains(@id,'ExportGridButton')]/span/*")).click();
-			logger.info("Clicked - Export Grid");
+			System.out.println("Clicked - Export Grid");
 		} catch (NoSuchElementException e1) {
 			e1.printStackTrace();
 			Thread.sleep(3000);
 			driver.findElement(By.xpath("//a[contains(@id,'ExportGridButton')]/span/*")).click();
-			logger.info("Clicked - Export Grid");
+			System.out.println("Clicked - Export Grid");
 		} catch (WebDriverException e) {
 			e.printStackTrace();
 			Thread.sleep(3000);
 			driver.findElement(By.xpath("//a[contains(@id,'ExportGridButton')]/span/*")).click();
-			logger.info("Clicked - Export Grid");
+			System.out.println("Clicked - Export Grid");
 		}
 
 		Thread.sleep(1000);
@@ -264,57 +221,19 @@ public class CommonCheney {
 		WebElement ddl_Excel = wait
 				.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[contains(.,'Excel')]"))));
 		ddl_Excel.click();
-		logger.info("format choosen as Excel");
+		System.out.println("format choosen as Excel");
 		// div[@id='ExportTypeContainer']/div/ul/li[2]/a/span
 
 		Thread.sleep(2000);
 
-		// Select columns for OG - Item no., Pack, Brand, Description, Price &
-		// caseUom
-
-		try {
-			Thread.sleep(2000);
-			ArrayList<String> removeColumns = new ArrayList<String>();
-			List<WebElement> OG_Col = driver.findElements(By.xpath("//ul[@id='Sortable']/*"));
-			logger.info(OG_Col.size());
-			String Col_id;
-			Iterator<WebElement> iterator = OG_Col.iterator();
-			while (iterator.hasNext()) {
-				WebElement element = (WebElement) iterator.next();
-				Thread.sleep(1000);
-
-				Col_id = element.getAttribute("id");
-				if (Col_id.equalsIgnoreCase("DistributorNumber") || Col_id.equalsIgnoreCase("Pack")
-						|| Col_id.equalsIgnoreCase("Brand") || Col_id.equalsIgnoreCase("Description")
-						|| Col_id.equalsIgnoreCase("CasePrice") || Col_id.equalsIgnoreCase("CaseUom")) {
-					logger.info("selected column :- " + Col_id);
-				}
-
-				else {
-					iterator.remove();
-					removeColumns.add(Col_id);
-				}
+		while (retry < maxretry) {
+			if (removeColumns() == true) {
+				break;
 			}
-
-			logger.info(removeColumns.size() + " and " + OG_Col.size());
-			Assert.assertEquals(OG_Col.size(), 6);
-
-			for (int i = 0; i < removeColumns.size(); i++) {
-				WebElement ll_removeCol = wait.until(ExpectedConditions.visibilityOf(driver.findElement(
-						By.xpath("//li[contains(@id,'" + removeColumns.get(i) + "')]/table/tbody/tr/td[2]/img"))));// remove
-				// column
-				ll_removeCol.click();
-				logger.info("removed column :- " + removeColumns.get(i));
-				Thread.sleep(3000);
-			}
-
-		} catch (StaleElementReferenceException e) {
-
-			e.printStackTrace();
-		} catch (Exception e) {
-			logger.info(e.getMessage());
-			e.printStackTrace();
 		}
+		List<WebElement> OG_Col = driver.findElements(By.xpath("//ul[@id='Sortable']/*"));
+		System.out.println(OG_Col.size());
+		Assert.assertEquals(OG_Col.size(), 7);
 
 		// Click Download Button
 		WebElement lnk_Download = wait.until(ExpectedConditions
@@ -332,4 +251,97 @@ public class CommonCheney {
 
 	}
 
+	public boolean removeColumns() {
+		// Select columns for OG - Item no., Pack, Brand, Description, Price &
+		// caseUom
+		try {
+			Thread.sleep(3000);
+			ArrayList<String> removeColumns = new ArrayList<String>();
+			List<WebElement> OG_Col = driver.findElements(By.xpath("//ul[@id='Sortable']/*"));
+			System.out.println(OG_Col.size());
+			String Col_id;
+			Iterator<WebElement> iterator = OG_Col.iterator();
+			while (iterator.hasNext()) {
+				WebElement element = (WebElement) iterator.next();
+				Thread.sleep(1000);
+
+				Col_id = element.getAttribute("id");
+				if (Col_id.equalsIgnoreCase("Sku") || Col_id.equalsIgnoreCase("DistributorNumber")
+						|| Col_id.equalsIgnoreCase("CaseUom") || Col_id.equalsIgnoreCase("UnitOfMeasure")
+						|| Col_id.equalsIgnoreCase("CasePack") || Col_id.equalsIgnoreCase("PackDescription")
+						|| Col_id.equalsIgnoreCase("Description") || Col_id.equalsIgnoreCase("ProductDescription")
+						|| Col_id.equalsIgnoreCase("BrandName") || Col_id.equalsIgnoreCase("Brand")
+						|| Col_id.equals("CatalogProductStatus") || Col_id.equalsIgnoreCase("ProductStatus")
+						|| Col_id.equalsIgnoreCase("CasePrice")) {
+					System.out.println("selected column :- " + Col_id);
+				}
+
+				else {
+					iterator.remove();
+					removeColumns.add(Col_id);
+				}
+			}
+
+			System.out.println("removeColumns - " + removeColumns.size() + " and OG_Col -" + OG_Col.size());
+
+			if (OG_Col.size() != 7) {
+				System.out.println("OG_Col.size - " + OG_Col.size());
+				addHiddenColumns();
+				return false;
+			}
+
+			for (String column : removeColumns) {
+				wait.until(ExpectedConditions
+						.elementToBeClickable(driver.findElement(By.xpath("//li[contains(@id,'" + column + "')]"))));// remove
+				// column
+				driver.findElement(By.xpath("//li[contains(@id,'" + column + "')]/table/tbody/tr/td[2]/img")).click();
+				Thread.sleep(3000);
+				System.out.println("removed column :- " + column);
+			} /*
+				 * for (int i = 0; i < removeColumns.size(); i++) {
+				 * wait.until(ExpectedConditions.elementToBeClickable(
+				 * driver.findElement(By.xpath("//li[contains(@id,'" + removeColumns.get(i) +
+				 * "')]"))));// remove // column driver.findElement(
+				 * By.xpath("//li[contains(@id,'" + removeColumns.get(i) +
+				 * "')]/table/tbody/tr/td[2]/img")) .click(); Thread.sleep(3000);
+				 * System.out.println("removed column :- " + removeColumns.get(i)); }
+				 */
+			return true;
+		} catch (StaleElementReferenceException e) {
+			e.printStackTrace();
+			return false;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	private void addHiddenColumns() {
+		try {
+			List<WebElement> OG_Col = driver.findElements(By.xpath("//table[@id='HiddenColumnsTable']/..//img"));
+			System.out.println(OG_Col.size());
+			String Col_id;
+			Iterator<WebElement> iterator = OG_Col.iterator();
+			while (iterator.hasNext()) {
+				WebElement element = (WebElement) iterator.next();
+				Thread.sleep(1000);
+				Col_id = element.getAttribute("id");
+				if (Col_id.equalsIgnoreCase("Sku") || Col_id.equalsIgnoreCase("DistributorNumber")
+						|| Col_id.equalsIgnoreCase("CaseUom") || Col_id.equalsIgnoreCase("UnitOfMeasure")
+						|| Col_id.equalsIgnoreCase("CasePack") || Col_id.equalsIgnoreCase("PackDescription")
+						|| Col_id.equalsIgnoreCase("Description") || Col_id.equalsIgnoreCase("ProductDescription")
+						|| Col_id.equalsIgnoreCase("BrandName") || Col_id.equalsIgnoreCase("Brand")
+						|| Col_id.equals("CatalogProductStatus") || Col_id.equalsIgnoreCase("ProductStatus")
+						|| Col_id.equalsIgnoreCase("CasePrice")) {
+					System.out.println("Added Column from Hidden column :- " + Col_id);
+					element.click();
+				} else {
+					System.out.println("Column not matched - " + Col_id);
+				}
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
